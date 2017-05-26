989e4f2752e341f091ed798075c84e1ff3ea4fe3
danilo marichal <danilo.marichal@icloud.com>
2017-05-26 12:34:18 -0400

    removed node modules

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'naranja0106@gmail.com',
-        pass: 'xxxxxxxxxx'
+        pass: 'Naranja6184'
     }
 });
   //Mail options
