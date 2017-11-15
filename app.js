c77317845e52a35fa18eb5c9d2193d546c0cbb93
danilo marichal <danilo.marichal@icloud.com>
2017-11-15 16:42:30 -0500

    makes it one vertical vertical site

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'naranja0106@gmail.com',
-        pass: 'Marcia6184'
+        pass: 'xxxxxxxx'
     }
 });
 
