ddac3f034f2e909b62c8e44f78a7319cb3c8bfb2
danilo marichal <danilo.marichal@icloud.com>
2017-11-15 16:41:39 -0500

    makes it one vertical vertical site

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const bodyParser = require("body-parser");
 const session = require('express-session');
 const methodOverride = require('method-override');
 var nodemailer = require("nodemailer");
-var smtp =require("smtp")
+var smtp =require("smtp");
+var cheerio = require('cheerio');
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
@@ -19,28 +20,31 @@ app.use(bodyParser.json());
 
 var db = pgp('postgres://rcoppa@localhost:5432/portfolio');
 
-app.get("/", function(req, res){
-  res.render("index");
-});
 
+/*
 app.get("/mywork", function(req, res){
   res.render("show");
 });
 
-/*app.get("/resume",function(req,res){
+app.get("/resume",function(req,res){
   res.render("resume");
-});*/
+});
 
 app.get("/contact",function(req,res){
   res.render("contact");
-});
+});*/
 
 app.get("/sent",function(req, res){
   res.render("sent");
 });
 
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+
 exports.contact = function(req, res){
-    res.render('contact', { title: 'Raging Flame Laboratory - Contact', page: 'contact' })
+    res.render('/contact');
 };
 
 app.post('/contact', function (req, res) {
@@ -51,12 +55,14 @@ let transporter = nodemailer.createTransport({
         pass: 'Marcia6184'
     }
 });
+
+
   //Mail options
   mailOpts = {
       from: req.body.email, //grab form data from the request body object
       to: 'naranja0106@gmail.com',
-      subject: req.body.name + " " + req.body.email,
-      text: req.body.message
+      subject: req.body.name,
+      text: req.body.name + " is reaching out to you."+"\n"+"\n"+"The email: "+"\n"+req.body.email+"\n"+"\n"+"\n"+"The Message:"+"\n"+"\n"+req.body.message
   };
 
 transporter.sendMail(mailOpts, (error, info) => {
@@ -64,13 +70,32 @@ transporter.sendMail(mailOpts, (error, info) => {
         return console.log(error);
     }
     console.log(info.messageId, info.response);
+    res.redirect("/sent");
+});
 
-    res.redirect("/sent")
+
+  mailOpts2 = {
+      from: req.body.email, //grab form data from the request body object
+      to: req.body.email,
+      subject:"Your message to Dan",
+      text: req.body.name + ","+"\n"+"\n"+" You said: "+"\n"+"\n" +req.body.message+"\n"+"\n"+"\n"+"\n"+"Thank you so much for your message! We'll be in touch soon..."+"\n"+"\n"+"Dan M."
+  };
+
+transporter.sendMail(mailOpts2, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log(info.messageId, info.response);
+
+    res.redirect("/sent");
 });
+
 });
+
+
 
 var port = process.env.PORT || 8000
 
 app.listen(port, function() {
-  console.log("Server Running {^-^}");
+  console.log("Server Running on port "+port+" {^-^}");
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,6 +6,14 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 };
 
+function openN() {
+   $("#myBurguer").css({'width':'50%'});
+};
+
+function closeN() {
+    $("#myBurguer").css({'width':'0%'});
+};
+
 var d = document;
 var wrap = d.querySelector('.wrap');
 var items = d.querySelector('.items');
@@ -29,12 +37,158 @@ function next() {
 }
 
 window.addEventListener('resize', setTransform);
-
 $('.home').hover(function(){
   $('#info1').text("home");
 })
 
 
+$('#game').on('mouseover',function(){
+$('#one').css({'width':'100%','transition':'1.5s'});
+$('#p1').css({'opacity':'1','transition':'4s'});
+$('#d1').css({'opacity':'1','transition':'4s'});
+});
 
+$('#game').on('mouseleave',function(){
+$('#one').css({'width':'30%','transition':'1.5s'});
+$('#p1').css({'opacity':'0','transition':'0.5s'});
+$('#d1').css({'opacity':'0','transition':'0.5s'});
+//$('#about_me').css({'opacity':'1','transition':'1.5s'});
+});
+
+$('#travel').on('mouseover',function(){
+$('#two').css({'width':'100%','transition':'1.5s'});
+$('#p2').css({'opacity':'1','transition':'4s'});
+$('#d2').css({'opacity':'1','transition':'4s'});
+});
+$('#travel').on('mouseleave',function(){
+$('#two').css({'width':'30%','transition':'1.5s'});
+$('#p2').css({'opacity':'0','transition':'0.5s'});
+$('#d2').css({'opacity':'0','transition':'0.5s'});
+});
+
+$('#activism').on('mouseover',function(){
+$('#three').css({'width':'100%','transition':'1.5s'});
+$('#p3').css({'opacity':'1','transition':'4s'});
+$('#d3').css({'opacity':'1','transition':'4s'});
+});
+$('#activism').on('mouseleave',function(){
+$('#three').css({'width':'30%','transition':'1.5s'});
+$('#p3').css({'opacity':'0','transition':'0.5s'});
+$('#d3').css({'opacity':'0','transition':'0.5s'});
+});
+
+$('#finance').on('mouseover',function(){
+$('#four').css({'width':'100%','transition':'1.5s'});
+$('#p4').css({'opacity':'1','transition':'4s'});
+$('#d4').css({'opacity':'1','transition':'4s'});
+});
+$('#finance').on('mouseleave',function(){
+$('#four').css({'width':'30%','transition':'1.5s'});
+$('#p4').css({'opacity':'0','transition':'0.5s'});
+$('#d4').css({'opacity':'0','transition':'0.5s'});
+});
+
+$(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+});
+
+$('.home').on('click',function(){
+ $('html,body').animate({
+        scrollTop: $('#index').offset().top
+      },3000);
+ return false;
+});
+
+$('.work').on('click',function(){
+ $('html,body').animate({
+        scrollTop: $('#contac').offset().top
+      },2000);
+ return false;
+});
+
+$('.contact').on('click',function(){
+ $('html,body').animate({
+        scrollTop: $('#contact_form').offset().top
+      },2000);
+ return false;
+});
+
+$('.inside_nav1').on('click',function(){
+ closeN();
+ $('html,body').animate({
+        scrollTop: $('#index').offset().top
+      },2000);
+ return false;
+});
+
+$('.inside_nav2').on('click',function(){
+ closeN();
+ $('html,body').animate({
+        scrollTop: $('#contac').offset().top
+      },2000);
+ return false;
+});
+
+$('.inside_nav3').on('click',function(){
+ closeN();
+ $('html,body').animate({
+        scrollTop: $('#contact_form').offset().top
+      },2000);
+ return false;
+});
+
+$('.inside_nav4').on('click',function(){
+ closeN();
+window.open('http://localhost:8000/files/resume.pdf');
+ if(window.open('http://localhost:8000/files/resume.pdf')===true){
+  link.focus();
+ }else{
+  alert("Please allow popups for this website");
+ }
+ return false;
+});
+
+$('.resume').on('click',function(){
+window.open('http://localhost:8000/files/resume.pdf');
+ if(window.open('http://localhost:8000/files/resume.pdf')===true){
+  link.focus();
+ }else{
+  alert("Please allow popups for this website");
+ }
+ return false;
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  $("#result").text("");
+  var email = $("#segundo").val();
+  if (validateEmail(email)) {
+    $("#result").text(email + " is valid :)");
+    $("#result").css("color", "#008180");
+  } else {
+    $("#result").text(email + " is not valid :(");
+    $("#result").css("color", "red");
+  }
+  return false;
+}
+
+$("#segundo").on("change", validate);
+
+
+$(window).on('load',function(){
+     $('#loader').fadeOut();
+});
+
+$('#subm').on('click', function(){
+  $('#loader').show();
+});
+
+$('#b').on('click',function(){
+  window.history.go(-1);
+});
 
 
